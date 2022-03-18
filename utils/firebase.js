8ee1ab92a8c8e346d1a4e889d530e6de1f9f89ff
JosephVillany <josephvillany@gmail.com>
2022-03-17 22:02:24 -0500

    controllers-function

---
+++ utils/firebase.js
@@ -0,0 +1,14 @@
+const { initializeApp } = require("firebase/app");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQO0hmdDDE6kklf1npNGrJ4fQIk2Larfo",
+  authDomain: "multer-project-2cd54.firebaseapp.com",
+  projectId: "multer-project-2cd54",
+  storageBucket: "multer-project-2cd54.appspot.com",
+  messagingSenderId: "756706653442",
+  appId: "1:756706653442:web:893ff4531ce27b1e2e3e1b",
+  measurementId: "G-6SZ7S5YBCL"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
